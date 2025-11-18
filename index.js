3254e45de78a01bd14536cf47daec6ddfe074c3b
Jann Bajade <jannalexandrei.campos@gmail.com>
2025-11-19 06:15:19 +0800

    added sticky navbar

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+
+
+//NAVBAR
+const stickyNav = document.getElementById('navbar');
+const mainHeader = document.getElementById('start');
+let lastScrollY = window.scrollY;
+
+const headerHeight = window.innerHeight;
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+    // Condition 1: Hide/Show logic
+    if (currentScrollY > headerHeight) {
+        // Only show/hide the sticky nav once we are past the main header
+        if (currentScrollY > lastScrollY && currentScrollY > headerHeight + 50) {
+            // Scrolling DOWN
+            stickyNav.classList.add('nav-hidden');
+        } else {
+            // Scrolling UP
+            stickyNav.classList.remove('nav-hidden');
+        }
+    } else {
+        // If we scroll back up into the main header area, keep the sticky nav hidden
+    }
+    lastScrollY = currentScrollY;
+});
+
+
+//SLIDESHOW
 const buttons = document.getElementsByClassName("slideshow_arrow");
 buttons[0].addEventListener("click", () => {
 	console.log("wow");
@@ -15,7 +43,7 @@ buttons[0].addEventListener("click", () => {
 });
 buttons[1].addEventListener("click", () => {
 	console.log("wow");
-	
+
 	const slides = document.querySelector(".carousel>ul");
 	const activeSlide = slides.querySelector("[data-active]");
 	let newIndex = [...slides.children].indexOf(activeSlide) + 1;

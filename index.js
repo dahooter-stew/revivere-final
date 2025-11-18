b0faefcce959288650029846aa343c6ec72f6e51
Jann Bajade <jannalexandrei.campos@gmail.com>
2025-11-19 07:23:15 +0800

    better navbar, added music, music toggle, and music volume

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', () => {
     // Condition 1: Hide/Show logic
     if (currentScrollY > headerHeight) {
         // Only show/hide the sticky nav once we are past the main header
-        if (currentScrollY > lastScrollY && currentScrollY > headerHeight + 50) {
+        if (currentScrollY > lastScrollY && currentScrollY > headerHeight) {
             // Scrolling DOWN
             stickyNav.classList.add('nav-hidden');
         } else {
@@ -51,4 +51,62 @@ buttons[1].addEventListener("click", () => {
 
 	slides.children[newIndex].dataset.active = true;
 	delete activeSlide.dataset.active;
+});
+
+
+//MUSIC
+
+
+
+const playlist = [
+	"City Ruins (Rays of Light)",
+	"Amusement Park",
+	"Ashes of Dreams (Aratanaru)",
+	"Blissful Death",
+	"City Ruins (Shade)",
+	"Faltering Prayer (Dawn Breeze)",
+	"Faltering Prayer (Starry Sky)",
+	"Fortress of Lies",
+	"Kaine Salvation",
+	"Peaceful Sleep",
+	"Repose",
+	"Shadowlords Castle (Memory)",
+	"Song of the Ancients (Devola)",
+	"Vague Hope (Cold Rain)",
+	"Voice of no Return "
+];
+let index = 0;
+let timeStamp = 0;
+let volume = 15;
+const musicToggle = document.getElementById("music_toggle");
+let musicVolume = document.getElementById("music_volume");
+const backgroundMusic = document.getElementById("background_music");
+
+function playNext()
+{
+	backgroundMusic.src = "./assets/background_music/" + playlist[index].toLowerCase().replace(/\s+/g, "_") + ".mp3";
+	backgroundMusic.currentTime = timeStamp;
+	backgroundMusic.volume = (volume / 100);
+	backgroundMusic.play();
+	index = (index + 1) % playlist.length;
+
+	console.log(backgroundMusic.src);
+	console.log("music playing", index - 1);
+}
+
+function playStop()
+{
+	timeStamp = backgroundMusic.currentTime;
+	backgroundMusic.pause();
+}
+
+musicVolume.addEventListener("input", () => {
+    volume = musicVolume.value;
+   	backgroundMusic.volume = (volume / 100);
+});
+
+backgroundMusic.addEventListener("ended", playNext);
+musicToggle.addEventListener("change", (event) => {
+	if (musicToggle.checked) playNext();
+	else 					 playStop();
 });

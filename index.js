d5d17ae90c7161834600ef2daf7edbc26db2e09f
Jann Bajade <jannalexandrei.campos@gmail.com>
2025-11-18 08:10:02 +0800

    marquee, finished first section

---
+++ index.js
@@ -0,0 +1,26 @@
+const buttons = document.getElementsByClassName("slideshow_arrow");
+buttons[0].addEventListener("click", () => {
+	console.log("wow");
+
+	const slides = document.querySelector(".carousel>ul");
+	const activeSlide = slides.querySelector("[data-active]");
+	let newIndex = [...slides.children].indexOf(activeSlide) - 1;
+	newIndex %= slides.children.length;
+	if (newIndex < 0) newIndex += slides.children.length;
+
+	console.log(newIndex)
+
+	slides.children[newIndex].dataset.active = true;
+	delete activeSlide.dataset.active;
+});
+buttons[1].addEventListener("click", () => {
+	console.log("wow");
+	
+	const slides = document.querySelector(".carousel>ul");
+	const activeSlide = slides.querySelector("[data-active]");
+	let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+	newIndex %= slides.children.length;
+
+	slides.children[newIndex].dataset.active = true;
+	delete activeSlide.dataset.active;
+});
